741e069844c8d5878d0c46c0e8ecc5da19e36b92
Dina Hafez <dinahafez.8@gmail.com>
2017-11-05 22:06:37 +0000

    Add mad libs script
    This is an online workshop practice in Treehouse. I just read the
    instructions and started to work alone and then in the second video I
    compared my result with the tutors result. My result was exactly the
    same as the tutor!

---
+++ mad_libs.rb
@@ -0,0 +1,14 @@
+
+print "Please enter a verb: "
+verb = gets.chomp
+print "Please enter a noun: "
+noun = gets.chomp
+print "Please enter an adjective: "
+adjective = gets.chomp
+print "Please enter another noun: "
+noun2 = gets.chomp
+
+puts "One day I decided to #{verb} in Web Development."
+puts "So I turned on my #{noun} and logged in to Treehouse."
+puts "Their teachers were really #{adjective}."
+puts "In no time, I'd learned to program a simple #{noun2}."
